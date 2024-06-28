abe71cc9648356b6876f580b6aec2a0b26041f8e
Riley Gavigan <rileygav@hotmail.com>
2024-06-27 21:08:24 -0400

    amazon version of Ball

--- Ball/Ball.swift
+++ Ball/Ball.swift
@@ -4,12 +4,12 @@ class Ball: SKNode {
     let id: String
 
     private let imgOffsetContainer = SKNode()
-    /**/ private let imgRotationContainer = SKNode()
-    /****/ private let img = SKSpriteNode(imageNamed: "Ball")
+    private let imgRotationContainer = SKNode()
+    private let img = SKSpriteNode(imageNamed: "Ball")
+    private let amazonImage = SKSpriteNode(imageNamed: "amzn") // New property
 
     let radius: CGFloat
 
-//    let view: NSHostingView<BallView<Circle>>
     private let shadowSprite = SKSpriteNode(imageNamed: "ContactShadow")
     private let shadowContainer = SKNode() // For fading in/out
 
@@ -25,8 +25,6 @@ class Ball: SKNode {
     }
 
     init(radius: CGFloat, pos: CGPoint, id: String) {
-//        self.view = NSHostingView(rootView: BallView(shape: Circle(), radius: radius, color: Color(hex: 0xF84E35)))
-//        self.view.frame = CGRect(x: 0, y: 0, width: radius * 2, height: radius * 2)
         self.id = id
         self.radius = radius
         super.init()
@@ -52,7 +50,11 @@ class Ball: SKNode {
 
         img.size = CGSize(width: radius * 2, height: radius * 2)
         imgRotationContainer.addChild(img)
-//        img.alpha = 0.01
+
+        amazonImage.position = CGPoint(x: 0, y: 0)
+        amazonImage.size = CGSize(width: radius * 1.5, height: radius * 2)
+        amazonImage.zPosition = 1 // Ensure the image is behind the ball
+        imgRotationContainer.addChild(amazonImage)
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -90,10 +92,27 @@ class Ball: SKNode {
 
         let targetScale = remap(x: strength, domainStart: 0, domainEnd: 1, rangeStart: 1, rangeEnd: 0.8)
         let velocity = remap(x: strength, domainStart: 0, domainEnd: 1, rangeStart: -5, rangeEnd: -10)
-//        squish.animate(toValue: targetScale, velocity: velocity, completion: nil)
 
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 0.01) {
-//            self.squish.animate(toValue: 1, velocity: self.squish.velocity, completion: nil)
-//        }
+        // Rotate the amazonImage differently based on the collision angle
+        let initialRotation = -angle * 0.5
+        amazonImage.zRotation = initialRotation
+
+        // Calculate the rotation speed and duration based on the collision strength
+        let rotationSpeed = CGFloat(remap(x: strength, domainStart: 0, domainEnd: 1, rangeStart: 0, rangeEnd: 2 * Double.pi))
+        let rotationDuration = remap(x: strength, domainStart: 0, domainEnd: 1, rangeStart: 1, rangeEnd: 3)
+
+        // Create and run the rotation action
+        let rotateAction = SKAction.rotate(byAngle: rotationSpeed, duration: rotationDuration)
+        amazonImage.run(rotateAction)
+
+        // squish.animate(toValue: targetScale, velocity: velocity, completion: nil)
+
+        // DispatchQueue.main.asyncAfter(deadline: .now() + 0.01) {
+        //     self.squish.animate(toValue: 1, velocity: self.squish.velocity, completion: nil)
+        // }
     }
+}
+
+func remap(x: Double, domainStart: Double, domainEnd: Double, rangeStart: Double, rangeEnd: Double) -> Double {
+    return rangeStart + (x - domainStart) * (rangeEnd - rangeStart) / (domainEnd - domainStart)
 }
